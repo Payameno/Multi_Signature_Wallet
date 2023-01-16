0ed0bd943944d2dc70ac7f3c6e0d62c0dcdb538e
Payam Beigi <beigi.payam@gmail.com>
2023-01-15 21:03:31 -0800

    Add a function to add transactions

--- MultiSig.sol
+++ MultiSig.sol
@@ -4,6 +4,15 @@ pragma solidity ^0.8.4;
 contract MultiSig {
     address[] public owners;
     uint256 public required;
+    uint public transactionCount;
+
+    struct Transaction {
+      address destination;
+      uint256 value;
+      bool executed;
+    }
+    //store transaction with an id untill confirmed by other members
+    mapping(uint => Transaction) public transactions;
 
     constructor(address[] memory _owners, uint256 _required) {
       require(_owners.length > 0);
@@ -11,5 +20,11 @@ contract MultiSig {
       require(_owners.length >= _required);
       owners = _owners;
       required = _required;
+    }
+
+    function addTransaction(address _destination, uint256 _value) public returns(uint transactionId) {
+      transactionId = transactionCount;
+      transactions[transactionCount] = Transaction(_destination, _value, false);
+      transactionCount++;
     }
 }
